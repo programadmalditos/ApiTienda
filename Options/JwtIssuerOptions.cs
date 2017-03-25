4f5ab2d3d319eed435532600823fe2bab5bcd3f1
Luis Felipe Gil Lamaignere <luisfelipegillamaignere@MacBook-Pro-de-Luis-2.local>
2017-03-25 21:21:20 +0100

    Actualizada version

--- Options/JwtIssuerOptions.cs
+++ Options/JwtIssuerOptions.cs
@@ -11,7 +11,7 @@ namespace ApiTienda.Options{
     public string Audience { get; set; }
     public DateTime NotBefore { get; set; } = DateTime.UtcNow;
     public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
-    public TimeSpan ValidFor { get; set; } = TimeSpan.FromMinutes(120);
+    public TimeSpan ValidFor { get; set; } = TimeSpan.FromMinutes(5);
     public DateTime Expiration => IssuedAt.Add(ValidFor);  
     public Func<Task<string>> JtiGenerator =>
       () => Task.FromResult(Guid.NewGuid().ToString());
